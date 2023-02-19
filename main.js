1d37e3fe3d0de19a8b2d6e51cbf623779740f10d
blubunny11 <85171252+blubunny11@users.noreply.github.com>
2023-02-19 11:52:46 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var noseX=0;
+var noseY=0;
+function setup() { 
+    canvas = createCanvas(300, 300); 
+    canvas.center(); video = createCapture(VIDEO); 
+    video.size(300, 300); video.hide(); 
+    poseNet = ml5.poseNet(video, modelLoaded); 
+    poseNet.on('pose', gotPoses); 
+} 
+    
+function modelLoaded() { 
+        console.log('PoseNet Is Initialized'); 
+    
+}
+
+function draw(){
+image(video, 0, 0, 300, 300);
+image(lipstick,noseX,noseY,50,20);
+}
+function take_snapshot(){
+save('imagelip.png');
+}
+function prelaod(){
+    lipstick=loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
+}
+function gotPoses(results){
+    if(results.length > 0 )
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+    }
+}
+
